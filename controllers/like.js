1435196444a44e93088ec613f2a170adc2cdb56c
yoonseooo71 <yoonseooo71@gmail.com>
2023-08-15 10:54:55 +0900

    databaseQuery 함수를 만들어서 코드 재활용

--- controllers/like.js
+++ controllers/like.js
@@ -1,45 +1,45 @@
 const { v4: uuidv4 } = require("uuid");
 const dbClient = require("../postgresql");
+const { databaseQuery } = require("../lib/database");
 
-function setPostLike(request, response) {
-  const { postId } = request.body;
-  const userId = request.user.id ; 
-  const postQuery = `insert into posts_likes ( post_id, user_id, regdate, updatedate, deletedate) values ($1, $2, CURRENT_TIMESTAMP, null, null)`;
-  dbClient.query(postQuery, [postId, userId], (err) => {
-    if (err) {
-      console.log(err);
-      response.status(500).send({ error: err });
-    } else {
-      response.status(200).send({ message: "success" });
-    }
-  });
+async function setPostLike(request, response) {
+  try {
+    const { postId } = request.body;
+    const userId = request.user.id ; 
+    const postQuery = `insert into posts_likes ( post_id, user_id, regdate, updatedate, deletedate) values ($1, $2, CURRENT_TIMESTAMP, null, null)`;
+    await databaseQuery(postQuery,[postId, userId]);
+    response.status(200).send({ message: "success" });
+  } catch(err) {
+    console.log("setPostLike:error:",err);
+    response.status(500).send({ error: err });
+  }
 }
-function deletePostLike(request,response){
-  const {postId} = request.body;
-  const userId = request.user.id ; 
-  const postQuery = `DELETE FROM posts_likes WHERE user_id = $1 and post_id = $2`
-  dbClient.query(postQuery,[userId,postId],(err) => {
-    if (err) {
-      console.log(err);
-      response.status(500).send({ error: err });
-    } else {
-      response.status(200).send({ message: "success" });
-    }
-  })
+async function deletePostLike(request,response){
+  try {
+    const {postId} = request.body;
+    const userId = request.user.id ; 
+    const postQuery = `DELETE FROM posts_likes WHERE user_id = $1 and post_id = $2`
+    await databaseQuery(postQuery,[userId,postId]) ; 
+    response.status(200).send({ message: "success" });
+  } catch(err) {
+    console.log("deletePostLike:error:",err);
+    response.status(500).send({ error: err });
+
+  }
 }
-function checkPostLike(request, response) {
-  const { postId } = request.params;
-  const userId = request.user.id ;
-  const postQuery = `SELECT EXISTS (SELECT 1 FROM posts_likes WHERE user_id = $1  AND post_id =  $2)`;
-  dbClient.query(postQuery, [userId,postId] , (err, result) => {
-    if (err) {
-      response.status(500).send({ error: err });
-    } else {
-      const exists = result.rows[0].exists;
-      const json = {isLike:exists};
-      response.status(200).json(json);
-    }
-  });
+async function checkPostLike(request, response) {
+  try {
+    const { postId } = request.params;
+    const userId = request.user.id ;
+    const postQuery = `SELECT EXISTS (SELECT 1 FROM posts_likes WHERE user_id = $1  AND post_id =  $2)`;
+    const result = await databaseQuery(postQuery,[userId,postId]); 
+    const isLike = result.rows[0].exists; 
+    const json = {isLike:isLike} ;
+    response.status(200).json(json);
+  } catch (err) {
+    console.log("checkPostLike:error:",err); 
+    response.status(500).send({error:err});
+  }
 }
 
 module.exports = {
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,130 +1,109 @@
+const { databaseQuery } = require("../lib/database");
 const dbClient = require("../postgresql");
 
-function getInfiniteScrollData(request, response) {
-  const limit = request.params.limit;
-  const offset = request.params.offset;
-  const postQuery = `
-    SELECT P.*, UD.name as writer, (SELECT COUNT(*) FROM posts_likes WHERE post_id = P.post_id) as likes
-    FROM posts as P 
-    INNER JOIN userdata as UD on P.uid = UD.id 
-    ORDER BY regdate asc 
-    LIMIT $1 OFFSET $2;
-  `;
-  dbClient.query(postQuery, [limit, offset], (err, result) => {
-    if (err) {
-      console.log(err);
-      response.status(500).send({ error: err });
-    } else {
-      response.status(200).json(result.rows);
-      /** 데이터 전달 딜레이 테스트 코드*/
-      // setTimeout(() => {
-      //   response.status(200).json(result.rows);
-      // }, 1000);
-    }
-  });
+async function getInfiniteScrollData(request, response) {
+  try {
+    const limit = request.params.limit;
+    const offset = request.params.offset;
+    const postQuery = `
+      SELECT P.*, UD.name as writer, (SELECT COUNT(*) FROM posts_likes WHERE post_id = P.post_id) as likes
+      FROM posts as P 
+      INNER JOIN userdata as UD on P.uid = UD.id 
+      ORDER BY regdate asc 
+      LIMIT $1 OFFSET $2;
+    `;
+    const result = await databaseQuery(postQuery,[limit,offset]); 
+    response.status(200).json(result.rows);
+  } catch(err) {
+    console.log("getInfiniteScrollData:error:",err);
+    response.status(500).send({ error: err });
+  }
 }
-function getPostInfo(request, response) {
-  const postId = request.params.postId;
-  const postQuery = `SELECT P.*, UD.name as writer FROM posts as P INNER JOIN userdata as UD on P.uid = UD.id where P.post_id = $1`;
-  const postQuery2 = `SELECT COUNT(*) FROM posts_likes WHERE post_id =  $1`;
-  dbClient.query(postQuery, [postId], (err, result) => {
-    if (err) {
-      console.log(err);
-      response.status(500).send({ error: err });
-    } else {
-      const postData = result.rows[0]; 
-      dbClient.query(postQuery2,[postId],(err, result) => {
-        if (err) {
-          console.log(err);
-          response.status(500).send({ error: err });
-        } else {
-          const count = parseInt(result.rows[0].count) ; 
-          const json = {...postData,likes:count};
-          response.status(200).json(json);
+async function getPostInfo(request, response) {
+  try {
+    const postId = request.params.postId;
+    const postQuery = `SELECT P.*, UD.name as writer FROM posts as P INNER JOIN userdata as UD on P.uid = UD.id where P.post_id = $1`;
+    const countQuery = `SELECT COUNT(*) FROM posts_likes WHERE post_id =  $1`;
+    const result = await databaseQuery(postQuery,[postId]) ; 
+    const postData = result.rows[0];
+    const countResult = await databaseQuery(countQuery,[postId]) ; 
+    const count = parseInt(countResult.rows[0].count) ; 
+    const json = {...postData,likes:count};
+    response.status(200).json(json);
+  } catch (err) {
+    console.log("getPostInfo:error:",err);
+    response.status(500).send({ error: err });
+  }
+}
+async function getComment(request, response) {
+  try {
+    const postId = request.params.postId ; 
+    const postQuery = `select * from comment where post_id = $1`;
+    const result = await databaseQuery(postQuery,[postId]); 
+    const data = result.rows.map((el)=>{return {...el,reply:[]}});
+    const comments = data.filter((el)=>el.parents_id === null);
+    const replyes = data.filter((el)=>el.parents_id !== null);
+    replyes.forEach((reply)=>{
+      comments.forEach(comment=>{
+        if(reply.parents_id === comment.comment_id) {
+          comment.reply.push(reply);
         }
-      });
-    }
-  });
-}
-function getComment(request, response) {
-  const postId = request.params.postId ; 
-  const postQuery = `select * from comment where post_id = $1`;
-  dbClient.query(postQuery,[postId],(err,result)=>{
-    if (err) {
-      response.status(500).send({ error: err });
-    } else {
-      const data = result.rows.map((el)=>{return {...el,reply:[]}});
-      const comments = data.filter((el)=>el.parents_id === null);
-      const replyes = data.filter((el)=>el.parents_id !== null);
-      replyes.forEach((reply)=>{
-        comments.forEach(comment=>{
-          if(reply.parents_id === comment.comment_id) {
-            comment.reply.push(reply);
-          }
-        }) 
-      })
-      response.status(200).json(comments);
-    }
-  })
+      }) 
+    })
+    response.status(200).json(comments);
+  } catch(err) {
+    console.log("getComment:error:",err);
+    response.status(500).send({error:err})
+  }
 }
 
-function getPostsCount(request, response) {
-  const postQury = `select count(*) from posts`;
-  dbClient.query(postQury, (err, result) => {
-    if (err) {
-      response.status(500).send({ error: err });
-    } else {
-      response.status(200).json(result.rows[0]);
-    }
-  });
+async function getPostsCount(request, response) {
+  try {
+    const postQury = `select count(*) from posts`;
+    const result = await databaseQuery(postQury) ;
+    response.status(200).json(result.rows[0]); 
+  } catch(err) {
+    console.log("getPostCount:error:",err);
+    response.status(500).send({ error: err });
+  }
 }
-function postWrite(req, res) {
-  const { title, optiona, optionb, totalvotes, avotes, bvotes} =
+async function postWrite(req, res) {
+  try {
+    const { title, optiona, optionb, totalvotes, avotes, bvotes} =
     req.body;
-  const postQuery = `insert into posts(uid,title,optiona,optionb,totalvotes,avotes,bvotes,regdate,updatedate,deletedate) values ($1,$2,$3,$4,$5,$6,$7,CURRENT_TIMESTAMP,null,null)`;
+    const postQuery = `insert into posts(uid,title,optiona,optionb,totalvotes,avotes,bvotes,regdate,updatedate,deletedate) values ($1,$2,$3,$4,$5,$6,$7,CURRENT_TIMESTAMP,null,null)`;
+    await databaseQuery(postQuery,[req.user.id, title, optiona, optionb, totalvotes, avotes, bvotes])
+    res.status(200).send({ message: "success" });
+  } catch(err) {
+    console.log("postWrite:error:",err);
+    res.status(500).send({ error: err });
 
-  dbClient.query(
-    postQuery,
-    [req.user.id, title, optiona, optionb, totalvotes, avotes, bvotes],
-    (err) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send({ error: err });
-      } else {
-        res.status(200).send({ message: "success" });
-      }
-    }
-  );
+  }
 }
-function commentWrite(request, response) {
-  const {
-    post_id,
-    comment_id,
-    writer_id,
-    writer,
-    contents,
-    parents_id,
-  } = request.body;
-  const postQuery = `insert into comment (post_id,comment_id,writer_id,writer,contents, regdate,updatedate,deletedate,parents_id) values($1,$2,$3,$4,$5,CURRENT_TIMESTAMP,null,null,$6)`;
-  dbClient.query(
-    postQuery,
-    [
+async function commentWrite(request, response) {
+  try {
+    const {
       post_id,
       comment_id,
       writer_id,
       writer,
       contents,
       parents_id,
-    ],
-    (err) => {
-      if (err) {
-        console.log(err);
-        response.status(500).send({ error: err });
-      } else {
-        response.status(200).send({ message: "success" });
-      }
-    }
-  );
+    } = request.body;
+    const postQuery = `insert into comment (post_id,comment_id,writer_id,writer,contents, regdate,updatedate,deletedate,parents_id) values($1,$2,$3,$4,$5,CURRENT_TIMESTAMP,null,null,$6)`;
+    await databaseQuery(postQuery,[
+      post_id,
+      comment_id,
+      writer_id,
+      writer,
+      contents,
+      parents_id,
+    ])
+    response.status(200).send({ message: "success" });
+  } catch(err){
+    console.log("commentWrite:error:",err)
+    response.status(500).send({ error:err });
+  }
 }
 
 
